3de321081affa1eaccc4b357d37fc5bf6d5903fb
blanch_b <blanch_b@etna-altenance.net>
2023-11-17 13:07:00 +0000

    add timeblock

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Routes,  Route,BrowserRouter as Router} from "react-router-dom"
 import { NavBar } from './components/NavBar';
 import { Shop } from './pages/shop/Shop';
 import { Cart } from './pages/cart/Cart';
+
 const App = () => {
-  return <Router>
-    <NavBar />
-    <Routes>
-      <Route path="/" element={<Shop />} />
-      <Route path="/cart" element={<Cart />}/>
-    </Routes>
-  </Router>
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 0);
+
+    return () => clearTimeout(timer); // nettoyer sur le démontage
+  }, []);
+
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path="/cart" element={<Cart />}/>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
