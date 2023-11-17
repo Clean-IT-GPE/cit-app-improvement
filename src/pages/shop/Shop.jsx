f72ff63214aa819f79f4e90fafe24cd537a6b670
blanch_b <blanch_b@etna-altenance.net>
2023-11-17 14:38:01 +0000

    add for loop error

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import { PRODUCTS } from '../../products'
 import { Product } from './Product'
-import * as echarts from 'echarts';
+import {Cpu} from 'phosphor-react'
+
+function badRules() {
+  for (let i = 0; i < 100; i++) {
+    for (let j = 0; j < 100; i++) {
+      let test =  i + j
+      for (let k = 0; k < 100; k++) {
+        test = test + k
+      }
+    }
+  }
+  console.log('badRules : ' + test)
+}
 
 export const Shop = () => {
+  badRules()
   return (
     <>
       <h1 className='mb-4 text-4xl font-extrabold leading-none font-custom tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white'>
